bb8217c1a002538215a126d80beaa2cbeced6b18
Mary Bellakbira <maryambellakbira@gmail.com>
2023-07-28 10:09:48 -0400

    Initial Commit

---
+++ This or That/This_or_ThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  This_or_ThatApp.swift
+//  This or That
+//
+//  Created by scholar on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct This_or_ThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
